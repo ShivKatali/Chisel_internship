066b16ed6d8bf6459211dc4257938fef77390dea
ShivKatali <44540988+ShivKatali@users.noreply.github.com>
2019-07-05 17:22:31 +0530

    Add files via upload

---
+++ src/main/scala/examples/Test_pwm.scala
@@ -0,0 +1,53 @@
+package examples
+
+import chisel3._
+import chisel3.util._
+
+
+
+
+class Test_pwm(val n :Int) extends Module {
+  val io = IO(new Bundle {
+    val rise  = Input(UInt(n.W))
+    val fall =  Input(UInt(n.W))
+    val out = Output(UInt(n.W))
+    val reset =Input(Bool())
+
+
+  })
+
+  val risereg= RegInit(0.U(n.W))
+  val fallreg= RegInit(0.U(n.W))
+  val counter=RegInit(1.U(n.W))
+
+ io.out := 0.U
+ risereg:= io.rise
+ fallreg := io.fall
+ 
+
+  when(!io.reset){
+          when(counter <= risereg)  {
+          counter := counter + 1.U
+          io.out := 1.U
+          }
+          .elsewhen(counter > risereg && counter < risereg+fallreg){
+            counter := counter + 1.U
+          	io.out := 0.U
+
+          
+          } .elsewhen(counter === risereg+fallreg)  {
+        	counter := 1.U 
+        	io.out := 0.U
+            
+                                             
+          }
+      }    
+   .elsewhen(io.reset)
+   {
+   	  counter := 0.U
+   }    
+
+}
+  
+
+
